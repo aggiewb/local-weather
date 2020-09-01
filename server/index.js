2652b7e2b86e19e22ab744cc2f490e4415cc951d
AggieWB <usmaggie23@gmail.com>
2020-08-31 21:59:30 -0700

    Add app.use() for 404 responses

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
 
-app.listen(3001, () => console.log('Express server started'));
+app.listen(5000, () => console.log('Express server started'));
 
 app.get('/:longitude/:latitude', (request, response) => {
     const longitude = request.params.longitude;
     const latitude = request.params.latitude;
 });
+
+app.use((request, response) => {
+    response.type('text/html');
+    response.status(404);
+    response.send('<h1>404 error</h1><p>Sorry! That page couldn\'t be found.</p>');
+})
