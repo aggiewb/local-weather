8bb5ebeed2b94249ac0ed2f0ad54d0d2c9406cfa
AggieWB <usmaggie23@gmail.com>
2020-08-31 21:47:23 -0700

    Add GET route '/:longitude/:latitude'

--- server/index.js
+++ server/index.js
@@ -1,4 +1,9 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000, () => console.log('Express server started'));
+app.listen(3001, () => console.log('Express server started'));
+
+app.get('/:longitude/:latitude', (request, response) => {
+    const longitude = request.params.longitude;
+    const latitude = request.params.latitude;
+});
