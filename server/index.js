38ceb665389153ca3e69376d420fd146bff8979d
AggieWB <usmaggie23@gmail.com>
2020-09-01 20:32:24 -0700

    Change parseInt to parseFloat, and remove console.log()

--- server/index.js
+++ server/index.js
@@ -5,14 +5,14 @@ const fetch = require('node-fetch');
 app.listen(5000, () => console.log('Express server started'));
 
 app.get('/:longitude/:latitude', (request, response) => {
-    const longitude = parseInt(request.params.longitude);
-    const latitude = parseInt(request.params.latitude);
-    console.log(longitude, latitude);
+    const longitude = parseFloat(request.params.longitude);
+    const latitude = parseFloat(request.params.latitude);
+    let tempCelsius = 0;
     fetch(`https://fcc-weather-api.glitch.me/api/current?lat=${latitude}&lon=${longitude}`)
     .then(fetchResponse => fetchResponse.json())
     .then(weatherJSON => {
         tempCelsius = weatherJSON.main.temp;
-        console.log(tempCelsius);
+        response.json({tempCelsius});
     });
 });
 
