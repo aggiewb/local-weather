fedf13c238ba71bbb29935310d1710d9a17550f1
AggieWB <usmaggie23@gmail.com>
2020-09-09 19:06:49 -0700

    Cache server and export to use in testing file
    Update conditional for an error response if bad params are passed in

--- server/index.js
+++ server/index.js
@@ -1,27 +1,23 @@
 const express = require('express');
 const app = express();
 const fetch = require('node-fetch');
-
-app.listen(5000, () => console.log('Express server started'));
+const server = app.listen(5000, () => console.log('Express server started'));
 
 app.get('/:longitude/:latitude', (request, response) => {
     const longitude = parseFloat(request.params.longitude);
     const latitude = parseFloat(request.params.latitude);
     fetch(`https://fcc-weather-api.glitch.me/api/current?lat=${latitude}&lon=${longitude}`)
-        .then(fetchResponse => {
-            if(fetchResponse.ok){
-                return fetchResponse.json();
-            } else {
-                throw new Error(fetchResponse.statusText);
-            }
-        })
+        .then(fetchResponse => fetchResponse.json())
         .then(weatherJSON => {
+            if(weatherJSON.error){
+                throw new Error(weatherJSON.error);
+            }
             const tempCelsius = weatherJSON.main.temp;
             const location = weatherJSON.name;
             const type = weatherJSON.weather[0].main;
             response.json({tempCelsius, location, type});
         })
-        .catch(error => response.json(error));
+        .catch(error => response.json({error}));
 });
 
 app.use((request, response) => {
@@ -29,3 +25,5 @@ app.use((request, response) => {
     response.status(404);
     response.send('<h1>404 error</h1><p>Sorry! That page couldn\'t be found.</p>');
 });
+
+module.exports = {server}
