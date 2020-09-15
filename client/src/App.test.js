a5bd16d656583a3a3b3169c3b897f66690379f9e
AggieWB <usmaggie23@gmail.com>
2020-09-15 16:38:41 -0700

    Update shallow render test to include all child components and
    their initial values
    Add test for CurrentLocation when the location prop is updated after set state is called

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -8,6 +8,38 @@ it('renders', () => {
 });
 
 //shallow renders the component but none of the React child components
-it('shallow render', () => {
-  shallow(<App />);
+it('should shallow render all child components, and initialize their props', () => {
+  const component = shallow(<App />);
+  const header = component.find('h1');
+  expect(header.exists()).toEqual(true);
+  expect(header.text()).toEqual('freeCodeCamp Take Home Projects - Show the Local Weather');
+  
+  const currentLocation = component.find('CurrentLocation');
+  expect(currentLocation.exists()).toEqual(true);
+  expect(currentLocation.prop('location')).toEqual('');
+
+  const temp = component.find('Temp');
+  expect(temp.exists()).toEqual(true);
+  expect(temp.prop('tempCelsius')).toEqual(0);
+  expect(temp.prop('tempFahrenheit')).toEqual(0);
+  expect(temp.prop('unit')).toEqual('');
+
+  const tempUnit = component.find('TempUnit');
+  expect(tempUnit.exists()).toEqual(true);
+  expect(tempUnit.prop('unit')).toEqual('');
+  expect(tempUnit.prop('unitToggle')).toBeDefined();
+
+  const weatherDescription = component.find('WeatherDescription');
+  expect(weatherDescription.exists()).toEqual(true);
+  expect(weatherDescription.prop('type')).toEqual('');
+
+  const footer = component.find('Footer');
+  expect(footer.exists()).toEqual(true);
+});
+
+it('should update the location of CurrentLocation when state location changes', () => {
+  const component = shallow(<App />);
+  const EXPECTED_LOCATION = 'Seattle, WA';
+  component.setState({location: EXPECTED_LOCATION});
+  expect(component.find('CurrentLocation').prop('location')).toEqual(EXPECTED_LOCATION);
 });
